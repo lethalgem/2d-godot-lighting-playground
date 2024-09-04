a39b81361d6e5d11c698425b878ade6ab4aa6aad
lethalgem <iwc123@gmail.com>
2024-09-03 21:48:28 -0400

    Have flashlight and engineer animation follow the mouse

--- 2d-godot-lighting-playground/scripts/engineer.gd
+++ 2d-godot-lighting-playground/scripts/engineer.gd
@@ -1,19 +1,15 @@
 class_name Engineer
 extends CharacterBody2D
 
+signal changed_direction_to(direction: Direction)
+
 @export var sprite: AnimatedSprite2D
 @export var speed: int = 250
 
-@onready var directions_traveled = Array()  # can assume never empty because we push a value on ready
+@onready var current_direction = Direction.Down
 @onready var current_state = state.Idle
 
-
-func _ready() -> void:
-	directions_traveled.push_front(direction.Down)
-	sprite.play("Idle Down")
-
-
-enum direction {
+enum Direction {
 	Up,
 	Down,
 	Left,
@@ -26,45 +22,34 @@ enum state {
 }
 
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("engineer_move_right"):
-		push(directions_traveled, direction.Right)
-	elif Input.is_action_just_pressed("engineer_move_left"):
-		push(directions_traveled, direction.Left)
-	elif Input.is_action_just_pressed("engineer_move_up"):
-		push(directions_traveled, direction.Up)
-	elif Input.is_action_just_pressed("engineer_move_down"):
-		push(directions_traveled, direction.Down)
+func _ready() -> void:
+	try_animate()
 
-	if (
-		directions_traveled.size() >= 2
-		and (
-			Input.is_action_pressed("engineer_move_right")
-			or Input.is_action_pressed("engineer_move_left")
-			or Input.is_action_pressed("engineer_move_up")
-			or Input.is_action_pressed("engineer_move_down")
-		)
-	):
-		if (
-			Input.is_action_just_released("engineer_move_right")
-			and directions_traveled[0] == direction.Right
-		):
-			pop_front(directions_traveled)
-		if (
-			Input.is_action_just_released("engineer_move_left")
-			and directions_traveled[0] == direction.Left
-		):
-			pop_front(directions_traveled)
-		if (
-			Input.is_action_just_released("engineer_move_up")
-			and directions_traveled[0] == direction.Up
-		):
-			pop_front(directions_traveled)
-		if (
-			Input.is_action_just_released("engineer_move_down")
-			and directions_traveled[0] == direction.Down
-		):
-			pop_front(directions_traveled)
+
+func _process(delta: float) -> void:
+	var vector_to_point = get_global_mouse_position() - global_position
+	var deg = rad_to_deg(vector_to_point.angle())
+
+	if deg > -45 && deg <= 45:
+		change_direction_to(Direction.Right)
+	elif deg >= 45 && deg < 135:
+		change_direction_to(Direction.Down)
+	elif deg >= 135 || deg < -135:
+		change_direction_to(Direction.Left)
+	else:
+		change_direction_to(Direction.Up)
+
+func change_direction_to(direction: Direction):
+	if direction != current_direction:
+		current_direction = direction
+		changed_direction_to.emit(direction)
+
+
+func _physics_process(delta: float):
+	get_input()
+	move_and_slide()
+	update_state()
+	try_animate()
 
 
 func get_input():
@@ -73,31 +58,16 @@ func get_input():
 	)
 	velocity = input_direction * speed
 
-
-func _physics_process(delta: float):
-	get_input()
-	move_and_slide()
+func update_state():
 	if velocity == Vector2(0, 0):
 		current_state = state.Idle
 	else:
 		current_state = state.Walk
 
-	try_animate()
-
-
-func push(array: Array, value: direction):
-	array.push_front(value)
-	array.resize(4)  # arbitrary to keep the vector from becoming a performance issue
-
-
-func pop_front(array: Array):
-	array.pop_front()
-
 
 func try_animate():
-	if directions_traveled.front() != null:
 		var animation_name = (
-			state.keys()[current_state] + " " + direction.keys()[directions_traveled.front()]
+			state.keys()[current_state] + " " + Direction.keys()[current_direction]
 		)
 		if animation_name != sprite.get_animation():
 			sprite.play(animation_name)
--- 2d-godot-lighting-playground/scripts/flashlight.gd
+++ 2d-godot-lighting-playground/scripts/flashlight.gd
@@ -3,3 +3,15 @@ extends Node2D
 
 func _process(delta: float) -> void:
 	look_at(get_global_mouse_position())
+
+
+func _on_engineer_changed_direction_to(direction: Engineer.Direction) -> void:
+	match direction:
+		Engineer.Direction.Up:
+			position = Vector2(10, 5)
+		Engineer.Direction.Down:
+			position = Vector2(-10, 5)
+		Engineer.Direction.Left:
+			position = Vector2(5, 10)
+		Engineer.Direction.Right:
+			position = Vector2(-5, 10)
